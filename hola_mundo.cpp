4185b6a1dd57ecef80e2b364e1a66d2157d6869a
Juan Pablo Castillo <juan.castillog@sansano.usm.cl>
2023-03-09 01:40:39 -0300

    corrección namespace

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-
+int a;
 /* Espacios de nombres, para evitar agregar std:: */
 using namespace std;
 
 /* Este programa imprime Hola mundo en pantalla */
 int main () {
-    // Usando namespace
-    std::cout << "Hola mundo" << std::endl;
+    // Sin usar "using namespace std;"
+    // std::cout << "Hola mundo" << std::endl;
     // Sin usar "using namespace std"
-    // cout << "Hola mundo" << endl;
-    /* Indica al sistema operativo si el programa finalizo correctamente
+    cout << "Hola mundo" << endl;
+    /* Valor de return indica al sistema operativo si el programa finalizo correctamente
         0:     El programa finaliza exitosamente
         != 0:  El programa no pudo finalizar correctamente (ERROR) */
     return 0;
